2f1b647f9fe5490918af086db171b99538e5ede3
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2022-03-03 21:57:22 +0530

    added constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-use std::env;
+use std::{env::{self, args}, fs};
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let config = Config::new(&args);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
