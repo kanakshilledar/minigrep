3a43d51826bee4eeaf2670797fe6e002d65a9f3c
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2022-03-03 22:12:00 +0530

    added basic error handling logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::{env::{self, args}, fs};
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing the arguments: {}", err);
+        process::exit(1);
+    });
 
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
@@ -16,9 +20,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
