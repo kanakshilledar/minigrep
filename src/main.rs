a3b2fc1092003d8a92f572767313d069ff3df5f0
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2022-03-04 21:50:41 +0530

    separated functions from  main.rs to lib.rs and added a test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{env::{self, args}, fs};
-use std::process;
+use std::{env, process};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,24 +8,9 @@ fn main() {
         process::exit(1);
     });
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+   if let Err(e) = minigrep::run(config) {
+       println!("Application error: {}", e);
 
-    println!("With text: \n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
+       process::exit(1);
+   } 
 }
